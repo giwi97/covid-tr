089797aa62d33e660738fedac0f4be814410b2da
giwi97 <giwanthawijekoon@gmail.com>
2021-05-14 17:18:34 +0530

    api fetch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,35 @@
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
-  components: {
-  }
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      loading: true,
+      title: 'data',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch(
+        "https://www.hpb.health.gov.lk/api/get-current-statistical"
+      );
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    
+    this.dataDate = data.data.update_date_time
+    this.stats = data.data
+    this.countries = data.data.hospital_data
+    this.loading = false
+  },
+};
 </script>
