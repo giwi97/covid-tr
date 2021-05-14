9d37e20b91428f92c3d3911b4014d3c48abb6b6a
giwi97 <giwanthawijekoon@gmail.com>
2021-05-14 13:05:26 +0530

    header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
-  <header>
-    <div>Covid-19 Tracker Sri Lanka</div>
+  <header class="text-center bg-green-500 text-white p-4 mb-10">
+    <div class="text-3xl md:text-3xl font-sans md:font-serif font-bold">Covid-19 Tracker Sri Lanka</div>
+    <div class="text-3md md:text-1md font-sans md:font-serif">Covid-19 ශ්‍රී ලංකා සංඛ්‍යා ලේඛන</div>
   </header>
 </template>
 
