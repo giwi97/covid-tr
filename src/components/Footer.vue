46bc70b3e56e2c9fe1acc73ad308cad5ad2e952b
giwi97 <giwanthawijekoon@gmail.com>
2021-05-14 13:26:24 +0530

    Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="text-center bg-green-500 text-white p-4 mt-10">
+    <div class="text-3md md:text-1md font-sans md:font-serif">
+      Made with &#9829; by GiWi
+    </div>
+    <p class="font-sans">Powered by <a class="text-blue-300" href="https://www.hpb.health.gov.lk">Health Promotion Burea Sri Lanka</a></p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
