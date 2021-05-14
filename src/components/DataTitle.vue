f05ed2fd581fdbcbc540847ce80d19bd7d053463
giwi97 <giwanthawijekoon@gmail.com>
2021-05-14 18:22:48 +0530

    date format

--- src/components/DataTitle.vue
+++ src/components/DataTitle.vue
@@ -4,14 +4,21 @@
       {{ text }}
     </h2>
     <div class="text-2xl mt-4 mb-10">
-      {{ dataDate }}
+      {{ timestamp }}
     </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   name: "DataTitle",
   props: ["text", "dataDate"],
+  computed: {
+      timestamp: function() {
+          return moment(this.dataDate).format('MMMM Do YYYY, h:mm:ss a')
+      }
+  }
 };
 </script>
